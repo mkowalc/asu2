4981b070c76a562700a93fd35075c95f19ebf914
root <root@localhost.localdomain>
2018-01-04 17:38:23 -0500

    2

--- asu2.pl
+++ asu2.pl
@@ -5,14 +5,31 @@ use warnings;
 use Switch;
 use Getopt::Std;
 
+#OBSLUGA Z POZIOMU TERMINALA
+
 my %options=();
-getopts("hj:ln:s:", \%options);
+getopts("ht", \%options);
 
 if ($options{h})
 {
   print_help();
 }
 
+if ($options{t})
+{
+  test();
+}
+
+
+sub test {
+  my ($name, $number) = @ARGV;
+
+  if(not defined $name) {
+    die "NEED NAME\n";
+  }
+
+  print "'$name' and '$number'\n";
+}
 
 sub print_help {
   print "POMOC\n";
