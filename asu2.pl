6df68f85240953a7da500ab674d9a69e085e5602
root <root@localhost.localdomain>
2018-01-14 11:54:59 -0500

    dodane pobieranie uidow

--- asu2.pl
+++ asu2.pl
@@ -4,33 +4,90 @@ use strict;
 use warnings;
 use Switch;
 use Getopt::Std;
+use IPC::Run 'run';
 
 #OBSLUGA Z POZIOMU TERMINALA
 
 my %options=();
-getopts("ht", \%options);
+getopts("ahi", \%options);
 
 if ($options{h})
 {
   print_help();
 }
 
-if ($options{t})
+if ($options{a})
 {
-  test();
+  add_user();
 }
 
+if ($options{i}){
+  get_all_uids();
 
-sub test {
-  my ($name, $number) = @ARGV;
+}
 
-  if(not defined $name) {
-    die "NEED NAME\n";
+sub add_user {
+  my ($username, $uid) = @ARGV;
+
+  if((not defined $username) or (not defined $uid)) {
+    die "Invalid parameters were given\n";
   }
 
-  print "'$name' and '$number'\n";
+  print "'$username' and '$uid'\n";
 }
+
+# sub test_ls {
+#   my ($command) = @ARGV;
+#
+#   system($command);
+# }
 
 sub print_help {
   print "POMOC\n";
+}
+
+sub get_users {
+	my @unames;
+	my $key;
+	open PASSWD, "/etc/passwd";
+		while(<PASSWD>) {
+    	my @f = split /:/;
+			push @unames, $f[0];
+    }
+	return @unames;
+}
+
+sub get_uid {
+    my $uid;
+    #$uid = system("id -u '$_[0]'");
+    run [ "id", "-u", "$_[0]" ], ">", \my $uid;
+    print ("DEBUG GETUID: $uid\n");
+    return $uid;
+}
+
+sub get_all_uids {
+
+  my @users;
+  @users = get_users();
+  my $uidxd;
+  for my $i (0 .. $#users)
+  {
+      $uidxd = get_uid($users[$i]);
+      
+  }
+}
+
+
+# sub display {
+# 	my @values;
+# 	my $key;
+# 	foreach $key (param) {
+# 		push @values, param($key);
+# 	}
+#   print ("processes of '$values[0]'...");
+# 		 system("ps -U $values[0]");
+#
+# }
+sub get_uids {
+  #TODO
 }
